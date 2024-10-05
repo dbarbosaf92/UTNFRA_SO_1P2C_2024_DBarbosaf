78e44c54ca3a0aaa76d6f62d0fe99b692e2c8361
dbarbosaf92 <dbarbosaf92@gmai.com>
2024-10-05 04:02:55 +0000

    agregando scrpts

---
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mkdir -p $HOME/Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}}
+
+echo
+echo "Mostramos el arbol de los directorios"
+tree Examenes-UTN
+
---
+++ RTA_SCRIPT_Examen_20241005/Punto_B.sh
@@ -0,0 +1,77 @@
+!#/bin/bash
+
+DISCO="/dev/sdc"
+echo "Creo las particiones"
+sudo fdisk $DISCO << EOF
+n
+p
+
+
++1G
+n
+p
+
+
++1G
+n
+p
+
+
++1G
+n
+e
+
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
+
+w
+EOF
+
+echo
+echo "mostrar particionamiento"
+sudo fdisk $DISCO -l
+
+echo
+echo "formateo las particiones"
+for i in {1..11}
+do
+        sudo mkfs.ext4 ${DISCO}$i
+done
+
+echo
+echo "monto particiones"
+sudo echo "${DISCO}1  $HOME/Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}2  $HOME/Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}3  $HOME/Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}5  $HOME/Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}6  $HOME/Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}7  $HOME/Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}8  $HOME/Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}9  $HOME/Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}10  $HOME/Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo echo "${DISCO}11  $HOME/Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+sudo mount -a
+
+echo
+echo "muestro particiones montadas"
+sudo df -h
+
+
