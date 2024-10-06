3927f2b6927e2fe144263cbbc834b62849e38f4d
dbarbosaf92 <dbarbosaf92@gmai.com>
2024-10-06 20:50:22 +0000

    agregando punto E

---
+++ RTA_SCRIPT_Examen_20241005/Punto_E.sh
@@ -0,0 +1,10 @@
+echo
+echo 'filtro basico'
+grep MemTotal /proc/meminfo > RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
+echo 'informacion chasis'
+sudo dmidecode -t chassis | grep Manufacturer >> RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
+
+echo
+echo 'Muestra archivo: '
+cat RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
+echo
