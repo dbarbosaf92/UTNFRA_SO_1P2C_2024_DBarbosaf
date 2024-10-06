e8f3fbeb68986e263fed74c540a1dcd2bcc83707
dbarbosaf92 <dbarbosaf92@gmai.com>
2024-10-06 21:11:47 +0000

    fixing punto E

--- RTA_SCRIPT_Examen_20241005/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_E.sh
@@ -1,7 +1,7 @@
 echo
 echo 'filtro basico'
 grep MemTotal /proc/meminfo > RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
-echo 'informacion chasis'
+echo 'informacion chasis' >> RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
 sudo dmidecode -t chassis | grep Manufacturer >> RTA_ARCHIVOS_Examen_20241005/Filtro_Basico.txt
 
 echo
