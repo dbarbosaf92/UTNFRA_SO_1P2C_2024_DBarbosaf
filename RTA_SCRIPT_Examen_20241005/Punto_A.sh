fd640fca8ca97982019b35a6ef588bc6e4da5687
dbarbosaf92 <dbarbosaf92@gmai.com>
2024-10-06 20:03:26 +0000

    fixing destinos de carpetas en los scripts A D

--- RTA_SCRIPT_Examen_20241005/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_A.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-mkdir -p $HOME/Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}}
+mkdir -p /Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}}
 
 echo
 echo "Mostramos el arbol de los directorios"
--- RTA_SCRIPT_Examen_20241005/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241005/Punto_D.sh
@@ -2,7 +2,7 @@
 
 echo
 echo 'Creo estructura asimetrica'
-mkdir -p Estructura_Asimetrica/{{correo/cartas{1..100},clientes/cartas{1..100},correo/carteros_{1..10}}
+mkdir -p $HOME/Estructura_Asimetrica/{{correo/cartas{1..100},clientes/cartas{1..100},correo/carteros_{1..10}}
 echo
 echo 'Muestro el tree:'
 tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
